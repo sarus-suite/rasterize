61f190eeb901c1d161b5274b2528b1826c5c5c9d
Matteo Chesi <chesi@cscs.ch>
2025-09-30 11:48:28 +0200

    VCUE-1081 add validate command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use raster::validate;
+use clap::{Parser, Subcommand};
+
+/// CLI tool for sarus-suite
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Validate EDF file 
+    Validate {
+        filepath: std::path::PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Validate { filepath } => validate(filepath),
+    }
 }
