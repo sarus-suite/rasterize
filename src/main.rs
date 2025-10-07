210e565f38e7192fe7ac8a2eeab9d1c10ded0169
Matteo Chesi <chesi@cscs.ch>
2025-10-07 17:14:53 +0200

    Add render function for testing purposes

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,12 @@ enum Command {
         #[arg(long, short, value_enum,default_value_t = FormatOutput::Text)]
         output: FormatOutput,
     },
+    /// Render EDF file 
+    Render {
+        filepath: String,
+        #[arg(long, short, value_enum,default_value_t = FormatOutput::Text)]
+        output: FormatOutput,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -79,11 +85,35 @@ fn validate(filepath: String, fout: FormatOutput) -> i32 {
     return out.return_code;
 }
 
+fn render(filepath: String, fout: FormatOutput) -> i32 {
+    let mut out = Out {
+        stdout: format!(""),
+        stderr: format!(""),
+        return_code: 0,
+    };
+
+    let ret = raster::render(filepath.clone());
+
+    match ret {
+        Ok(o) => {
+            out.stdout = format!("{}", serde_json::to_string_pretty(&o).unwrap_or(String::from("ERROR")));
+        },
+        Err(e) => {
+            out.stdout = format!("");
+            out.stderr = format!("{e}");
+            out.return_code = 1;
+        }
+    }
+    printout(fout, out.clone());
+    return out.return_code;
+}
+
 fn main() {
     let args = Args::parse();
 
     let rc = match args.command {
         Command::Validate { filepath, output } => validate(filepath, output),
+        Command::Render { filepath, output } => render(filepath, output),
     };
 
     std::process::exit(rc);
